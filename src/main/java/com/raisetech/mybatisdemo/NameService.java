07529ae5ab9c98215f27d31c3a4ca1be032040a7
Oonishi228 <oasttsv8@gmail.com>
2022-11-04 01:57:15 +0900

    Serviceの実装

---
+++ src/main/java/com/raisetech/mybatisdemo/NameService.java
@@ -0,0 +1,11 @@
+package com.raisetech.mybatisdemo;
+
+import java.util.List;
+
+public interface NameService {
+
+    List<Name> findAll();
+//    Name findById(int id) throws Exception;
+//    void create(String name);
+//    void update(int id, String name) throws Exception;
+}
