e7bb0b04745fd91311b548cfeeab165451d8d0ea
Oonishi228 <oasttsv8@gmail.com>
2022-11-16 23:35:08 +0900

    テーブル名をnamesからusersに変更

--- src/main/java/com/raisetech/mybatisdemo/UserMapper.java
+++ src/main/java/com/raisetech/mybatisdemo/UserMapper.java
@@ -10,24 +10,24 @@ import java.util.Optional;
 
 @Mapper
 public interface UserMapper {
-    @Select("SELECT * FROM names")
+    @Select("SELECT * FROM users")
     List<User> findAll();
 
-    @Select("SELECT * FROM names WHERE id = #{id}")
+    @Select("SELECT * FROM users WHERE id = #{id}")
     Optional<User> findById(int id);
 
-    @Select("SELECT * FROM names WHERE name = #{name}")
+    @Select("SELECT * FROM users WHERE name = #{name}")
     List<User> findByName(String name);
 
-    @Select("SELECT * FROM names WHERE residence = #{residence}")
+    @Select("SELECT * FROM users WHERE residence = #{residence}")
     List<User> findByResidence(String residence);
 
-    @Select("SELECT * FROM names WHERE name = #{name} AND residence = #{residence}")
+    @Select("SELECT * FROM users WHERE name = #{name} AND residence = #{residence}")
     List<User> findByNameAndResidence(String name, String residence);
 
-    @Insert("insert into names (name, residence) values (#{name}, #{residence})")
+    @Insert("insert into users (name, residence) values (#{name}, #{residence})")
     void createUser(User user);
 
-    @Delete("DELETE FROM names WHERE id = #{id}")
+    @Delete("DELETE FROM users WHERE id = #{id}")
     void deleteById(int id);
 }
