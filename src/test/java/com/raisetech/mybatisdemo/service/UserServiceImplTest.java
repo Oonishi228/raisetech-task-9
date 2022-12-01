4b88f439d5cbcc0f8fe329a79aef9d96c92c6902
Oonishi228 <oasttsv8@gmail.com>
2022-12-01 23:30:01 +0900

    存在するユーザーのIDを指定したときに正常にユーザーが返されるテストコードを追加

---
+++ src/test/java/com/raisetech/mybatisdemo/service/UserServiceImplTest.java
@@ -0,0 +1,35 @@
+package com.raisetech.mybatisdemo.service;
+
+import com.raisetech.mybatisdemo.entity.User;
+import com.raisetech.mybatisdemo.mapper.UserMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceImplTest {
+
+    @InjectMocks
+    UserServiceImpl userServiceImpl;
+
+    @Mock
+    UserMapper userMapper;
+
+    @Test
+    public void 存在するユーザーのIDを指定したときに正常にユーザーが返されること() throws Exception {
+        doReturn(Optional.of(new User(1, "koyama", "tokyo"))).when(userMapper).findById(1);
+
+        User actual = userServiceImpl.findById(1);
+        assertThat(actual.getId()).isEqualTo(1);
+        assertThat(actual.getName()).isEqualTo("koyama");
+        assertThat(actual.getResidence()).isEqualTo("tokyo");
+
+    }
+}
